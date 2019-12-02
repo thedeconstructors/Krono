20fae48d64ae5a825a988f6030e9f8f14322278f
Michael Remley <Mikejamesremley@gmail.com>
2019-12-01 23:20:51 -0800

    Added AWS Amplify Framework
    Need help solving crash oncreate() method upon AWSAppSyncClient()

--- KronoApp/app/src/main/java/com/example/krono/MainActivity.kt
+++ KronoApp/app/src/main/java/com/example/krono/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.krono
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.amazonaws.mobile.config.AWSConfiguration
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient
+
 
 const val EXTRA_MESSAGE = "com.example.krono.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
+    private var mAWSAppSyncClient: AWSAppSyncClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //var mAWSAppSyncClient = AWSAppSyncClient.builder().context(applicationContext).awsConfiguration(
+        //   AWSConfiguration(applicationContext)).build()
     }
 
     /*Called when the user taps the Search button*/
