a0e16f2d13a365c659202816226b3c3834f8b91c
BuildTools <unconfigured@null.spigotmc.org>
2019-12-02 20:40:54 -0800

    Added Java project for Krono

---
+++ KronoJava/app/src/main/java/com/deconstructors/krono/MainActivity.java
@@ -0,0 +1,14 @@
+package com.deconstructors.krono;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
