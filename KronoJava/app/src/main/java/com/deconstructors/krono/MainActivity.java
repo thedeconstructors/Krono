a73b6727b6d6b65440ebfc984a0ee3a56d259b7e
BuildTools <unconfigured@null.spigotmc.org>
2019-12-02 23:46:13 -0800

    Have user search and display information working

--- KronoJava/app/src/main/java/com/deconstructors/krono/MainActivity.java
+++ KronoJava/app/src/main/java/com/deconstructors/krono/MainActivity.java
@@ -2,13 +2,30 @@ package com.deconstructors.krono;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String USER_NAME = "com.deconstructors.krono.USER_NAME";
+    // Access a Cloud Firestore instance from your Activity
+    public static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnDisplayPersonClick(View view){
+        Intent intent = new Intent(this, DisplayPlansActivity.class);
+        intent.putExtra(USER_NAME, ((EditText)findViewById(R.id.txtPersonName)).getText().toString());
+        startActivity(intent);
     }
 }
