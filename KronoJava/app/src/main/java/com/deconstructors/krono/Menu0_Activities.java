9ac602b8616799e01074236c08354c91acbd1737
Jihyuk Chung <jihyuk.chung@oit.edu>
2020-01-20 19:50:44 -0800

    Developed the Basic Menu UI
    Added Custom Assets, Created 6 Menu Items, Created Layouts and Click Events for Some of the Menu Items, Changed Min API from 19 to 21 (bc it was not registered properly and was conflicted my code), Moved the Original Demo Files to Menu 4 Layout (Users)

---
+++ KronoJava/app/src/main/java/com/deconstructors/krono/Menu0_Activities.java
@@ -0,0 +1,14 @@
+package com.deconstructors.krono;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu0_Activities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu0__activities);
+    }
+}
---
+++ KronoJava/app/src/main/java/com/deconstructors/krono/Menu1_Plans.java
@@ -0,0 +1,14 @@
+package com.deconstructors.krono;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu1_Plans extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu1__plans);
+    }
+}
