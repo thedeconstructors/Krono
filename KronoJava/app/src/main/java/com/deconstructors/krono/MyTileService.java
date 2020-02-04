ccf28fb36e0f297e750f066945f0ebb58eefd146
Jihyuk Chung <jihyuk.chung@oit.edu>
2020-02-04 15:49:32 -0800

    Quick Tile Settings

---
+++ KronoJava/app/src/main/java/com/deconstructors/krono/MyTileService.java
@@ -0,0 +1,43 @@
+package com.deconstructors.krono;
+
+import android.graphics.drawable.Icon;
+import android.os.Build;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class MyTileService extends TileService
+{
+    private static final String _Tag = TileService.class.getSimpleName();
+    private Tile _tile = null;
+
+    @Override
+    public void onTileAdded()
+    {
+        super.onTileAdded();
+        Log.d(_Tag, "onTileAdded");
+    }
+
+    @Override
+    public void onStartListening()
+    {
+        super.onStartListening();
+        _tile = getQsTile();
+        Log.d(_Tag, "onStartListening: " + _tile.getLabel());
+    }
+
+    @Override
+    public void onClick()
+    {
+        super.onClick();
+        Log.d(_Tag, "onClick: " + Integer.toString(getQsTile().getState()));
+        _tile.setState(Tile.STATE_ACTIVE);
+        _tile.setIcon(Icon.createWithResource(getApplicationContext(), R.drawable.tileservice_addactivity));
+        _tile.updateTile();
+
+        // Call Add Activity
+    }
+}
