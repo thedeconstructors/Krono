d6d0be758d357bf8aed04f654e37fcea6d7d4e02
jared glasser <Jaredlightbulb@gmail.com>
2020-02-05 19:06:25 -0800

    Cleaned up some code and made the UpdateUsersToPlans() function.
    WARNING: UpdateUsersToPlans currently uses the GetUserID as the value to store both the planID and userID. Will update with proper planID once I understand how to do that.

--- KronoJava/app/src/main/java/com/deconstructors/krono/NewPlan.java
+++ KronoJava/app/src/main/java/com/deconstructors/krono/NewPlan.java
@@ -2,7 +2,6 @@ package com.deconstructors.krono;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,8 +23,6 @@ public class NewPlan extends AppCompatActivity
     private TextView startTime;
     private RecyclerView activities;
 
-    Map<String, Object> userPlan = new HashMap<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -33,26 +30,30 @@ public class NewPlan extends AppCompatActivity
         setContentView(R.layout.activity_menu1_plans_newplan);
 
         title = findViewById(R.id.txtTitle);
+        startTime = findViewById(R.id.txtStartTime);
     }
 
     public void createPlanOnClick(View view)
     {
+        Map<String, Object> userPlan = new HashMap<>();
+
         Toast emptyTextFailureMessage = Toast.makeText(NewPlan.this, "Must Enter All Text Fields", Toast.LENGTH_SHORT);
 
         if (title.getText().toString() != null && startTime.getText().toString() != null)
         {
             userPlan.put("title", title.getText().toString());
-            userPlan.put("starTime", startTime.getText().toString());
+            userPlan.put("startTime", startTime.getText().toString());
 
             final Toast successMessage = Toast.makeText(NewPlan.this, "Plan Added Successfully.", Toast.LENGTH_SHORT);
             final Toast failureMessage = Toast.makeText(NewPlan.this, "Error: Could Not Add Plan.", Toast.LENGTH_SHORT);
 
             FirebaseFirestore db = FirebaseFirestore.getInstance();
-            db.collection("useractivities")
+            db.collection("userplans")
                     .add(userPlan)
                     .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                         @Override
                         public void onSuccess(DocumentReference documentReference) {
+                            UpdateUsersToPlans();
                             successMessage.show();
                             finish();
                         }
@@ -68,5 +69,18 @@ public class NewPlan extends AppCompatActivity
         {
             emptyTextFailureMessage.show();
         }
+    }
+
+    public void UpdateUsersToPlans()
+    {
+        Map<String, Object> userToPlan = new HashMap<>();
+
+        //Currently using GetUserID() as the value for planID until I understand what we're supposed to be using as the planID
+        userToPlan.put("planid", SessionData.GetInstance().GetUserID());
+        userToPlan.put("userid", SessionData.GetInstance().GetUserID());
+
+        FirebaseFirestore db2 = FirebaseFirestore.getInstance();
+        db2.collection("userstoplans")
+                .add(userToPlan);
     }
 }
