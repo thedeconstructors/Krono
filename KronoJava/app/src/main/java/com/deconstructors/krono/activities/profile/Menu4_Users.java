cd871ae3a9f293eba1feff6a63381700277e5875
Jihyuk Chung <51769097+jihyukchung@users.noreply.github.com>
2020-02-20 01:43:24 -0800

    Users Page Now Displays User Name

--- KronoJava/app/src/main/java/com/deconstructors/krono/activities/profile/Menu4_Users.java
+++ KronoJava/app/src/main/java/com/deconstructors/krono/activities/profile/Menu4_Users.java
@@ -1,5 +1,6 @@
 package com.deconstructors.krono.activities.profile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,23 +11,61 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.deconstructors.krono.R;
+import com.deconstructors.krono.helpers.SessionData;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Menu4_Users extends AppCompatActivity
 {
     public static final String USER_NAME = "com.deconstructors.krono.USER_NAME";
 
+    private EditText _firstNameField;
+    private EditText _lastNameField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu4__users);
+        _firstNameField = findViewById(R.id.inputFirstName);
+        _lastNameField = findViewById(R.id.inputLastName);
+
+        GetUserInfo();
+    }
+
+    private void GetUserInfo()
+    {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference activitiesCollectionRef = db.collection("users");
+
+        DocumentReference docRef = db
+                .collection("users")
+                .document(SessionData.GetInstance().GetUserID());
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task)
+            {
+                if (task.isSuccessful())
+                {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists())
+                    {
+                        _firstNameField.setText(document.get("firstname").toString());
+                        _lastNameField.setText(document.get("lastname").toString());
+                    }
+                }
+            }
+        });
     }
 
     public void btnDisplayPersonClick(View view)
     {
-        /*Intent intent = new Intent(this, DisplayPlansActivity.class);
-        intent.putExtra(USER_NAME, ((EditText)findViewById(R.id.txtPersonName)).getText().toString());
-        startActivity(intent);*/
+
     }
 }
