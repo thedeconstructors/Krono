e1940f3aeaa2c56dc4e88bb0cce18f0e491d2a58
Jihyuk Chung <jihyuk.chung@oit.edu>
2020-03-01 05:39:44 -0800

    NavigationView (without DB) Demo

---
+++ PlanActivityUserDemo/app/src/main/java/com/demo/planactivityuserdemo/userinterface/ActivitiesFragment.java
@@ -0,0 +1,8 @@
+package com.demo.planactivityuserdemo.userinterface;
+
+import androidx.fragment.app.Fragment;
+
+public class ActivitiesFragment extends Fragment
+{
+
+}
--- PlanActivityUserDemo/app/src/main/java/com/demo/planactivityuserdemo/userinterface/MainActivity.java
+++ PlanActivityUserDemo/app/src/main/java/com/demo/planactivityuserdemo/userinterface/MainActivity.java
@@ -1,18 +1,25 @@
 package com.demo.planactivityuserdemo.userinterface;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -20,9 +27,9 @@ import android.view.inputmethod.EditorInfo;
 import com.demo.planactivityuserdemo.R;
 import com.demo.planactivityuserdemo.adapter.PlansRecyclerAdapter;
 import com.demo.planactivityuserdemo.model.Plan;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -36,20 +43,21 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity
         implements View.OnClickListener,
-                   PlansRecyclerAdapter.PlansRecyclerClickListener
+                   PlansRecyclerAdapter.PlansRecyclerClickListener, com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 {
     // Logcat
     private static final String TAG = "PlansActivity";
 
     // XML Widgets
     private Toolbar Toolbar;
-    private RecyclerView RecyclerView;
     private DrawerLayout DrawerLayout;
+    private NavigationView NavigationView;
     private ActionBarDrawerToggle DrawerToggle;
+    private AppBarConfiguration AppBarConfig;
 
     // Vars
     private ArrayList<Plan> PlanList = new ArrayList<>();
-    private Set<String> PlanListIDs = new HashSet<>();
+    private Set<String> PlanIDList = new HashSet<>();
     private PlansRecyclerAdapter RecyclerAdapter;
     private FirebaseFirestore FirestoreDB;
     private ListenerRegistration PlansEventListener;
@@ -61,37 +69,35 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.ui_main);
 
         setContentViews();
-        setRecyclerView();
+        //setRecyclerView();
     }
 
     private void setContentViews()
     {
-        // Toolbar & Navigation Drawer
+        // Toolbar
         this.Toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(this.Toolbar);
+        getSupportActionBar().setTitle(R.string.navigation_allActivitiesText);
+
+        // Navigation Drawer
         this.DrawerLayout = findViewById(R.id.main_drawerLayout);
         this.DrawerToggle = new ActionBarDrawerToggle(this,
                                                       this.DrawerLayout,
                                                       this.Toolbar,
                                                       R.string.navigation_openText,
                                                       R.string.navigation_closeText);
-        this.DrawerLayout.addDrawerListener(this.DrawerToggle);
+        this.DrawerLayout.addDrawerListener(DrawerToggle);
+        this.DrawerToggle.syncState();
+        this.NavigationView = findViewById(R.id.main_navigationView);
+        this.NavigationView.setNavigationItemSelectedListener(this);
+        this.NavigationView.setCheckedItem(R.id.nav_allActivities);
 
-        // Recycler View & Database
-        this.RecyclerView = findViewById(R.id.main_planRecyclerview);
+        // Database
         this.FirestoreDB = FirebaseFirestore.getInstance();
     }
 
-    private void setRecyclerView()
-    {
-        this.RecyclerAdapter = new PlansRecyclerAdapter(PlanList, this);
-        this.RecyclerView.setHasFixedSize(true);
-        this.RecyclerView.setAdapter(RecyclerAdapter);
-        this.RecyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
-
     /************************************************************************
-     * Purpose:         OnClickListener & Database
+     * Purpose:         Navigation View Sync and Toolbar Icon Animation
      * Precondition:    .
      * Postcondition:   .
      ************************************************************************/
@@ -102,12 +108,23 @@ public class MainActivity extends AppCompatActivity
         this.DrawerToggle.syncState();
     }
 
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        this.DrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /************************************************************************
+     * Purpose:         Database
+     * Precondition:    .
+     * Postcondition:   .
+     ************************************************************************/
     private void getPlans()
     {
-        CollectionReference planRef = FirestoreDB
-                .collection(getString(R.string.collection_users))
-                .document(FirebaseAuth.getInstance().getUid())
-                .collection(getString(R.string.collection_plans));
+        Query planRef = this.FirestoreDB
+                .collection(getString(R.string.collection_plans))
+                .whereEqualTo("ownerID", FirebaseAuth.getInstance().getCurrentUser().getUid());
 
         PlansEventListener = planRef.addSnapshotListener(new EventListener<QuerySnapshot>()
         {
@@ -121,19 +138,16 @@ public class MainActivity extends AppCompatActivity
                     return;
                 }
 
-
                 if(documentSnapshots != null)
                 {
                     for (QueryDocumentSnapshot doc : documentSnapshots)
                     {
-
-                        doc.getReference().getParent();
-
+                        //doc.getReference().getParent();
                         Plan plan = doc.toObject(Plan.class);
 
-                        if(!PlanListIDs.contains(plan.getPlanID()))
+                        if(!PlanIDList.contains(plan.getPlanID()))
                         {
-                            PlanListIDs.add(plan.getPlanID());
+                            PlanIDList.add(plan.getPlanID());
                             PlanList.add(plan);
                         }
                     }
@@ -176,11 +190,11 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        //MenuInflater inflater = getMenuInflater();
-        //inflater.inflate(R.menu.menu_plans, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_plans, menu);
 
         // Search Button
-        /*MenuItem searchItem = menu.findItem(R.id.plansMenu_search);
+        MenuItem searchItem = menu.findItem(R.id.plansMenu_search);
         SearchView searchView = (SearchView) searchItem.getActionView();
         searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
 
@@ -199,7 +213,7 @@ public class MainActivity extends AppCompatActivity
                 //RecyclerAdapter.resetFilterList();
                 return false;
             }
-        });*/
+        });
 
         return true;
     }
@@ -213,7 +227,7 @@ public class MainActivity extends AppCompatActivity
     protected void onResume()
     {
         super.onResume();
-        getPlans();
+        //getPlans();
     }
 
     @Override
@@ -230,5 +244,23 @@ public class MainActivity extends AppCompatActivity
     public void onClick(View view)
     {
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        if (item.getItemId() == R.id.nav_allActivities)
+        {
+            getSupportActionBar().setTitle(R.string.navigation_allActivitiesText);
+        }
+        else if (item.getItemId() == R.id.nav_friends)
+        {
+            getSupportActionBar().setTitle(R.string.navigation_friendsText);
+        }
+
+        DrawerLayout drawer = findViewById(R.id.main_drawerLayout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
