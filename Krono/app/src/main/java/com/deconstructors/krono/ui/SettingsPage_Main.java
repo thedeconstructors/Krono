4c6a01a65110f13050f600ad8c7135747b5d9317
Michael Remley <Mikejamesremley@gmail.com>
2020-04-28 14:28:11 -0700

    Settings Demo

--- Krono/app/src/main/java/com/deconstructors/krono/ui/SettingsPage_Main.java
+++ Krono/app/src/main/java/com/deconstructors/krono/ui/SettingsPage_Main.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.deconstructors.krono.R;
+import com.deconstructors.krono.utility.SettingsFragment;
 
 import javax.annotation.Nullable;
 
@@ -14,6 +15,9 @@ public class SettingsPage_Main extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_main);
-
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, new SettingsFragment())
+                .commit();
     }
 }
