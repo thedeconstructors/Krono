a67a2e9348a28995de6df61ad7a5c1aae049898f
Michael Remley <Mikejamesremley@gmail.com>
2020-05-16 20:50:21 -0700

    unknown changes

--- Krono/app/src/main/java/com/deconstructors/krono/ui/SettingsPage_Main.java
+++ Krono/app/src/main/java/com/deconstructors/krono/ui/SettingsPage_Main.java
@@ -18,8 +18,6 @@ public class SettingsPage_Main extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_main);
-
-
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings_container, new SettingsFragment())
