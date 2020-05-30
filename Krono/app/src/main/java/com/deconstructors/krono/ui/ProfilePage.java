737c70c2eaffa5edbe93cf275ec7e221aeadccd8
Michael Remley <Mikejamesremley@gmail.com>
2020-05-30 12:29:01 -0700

    Fixed API issue, and changed settings page

--- Krono/app/src/main/java/com/deconstructors/krono/ui/ProfilePage.java
+++ Krono/app/src/main/java/com/deconstructors/krono/ui/ProfilePage.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewDebug;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -79,11 +80,14 @@ public class ProfilePage extends AppCompatActivity {
                             EmailTextView.setText(Objects.requireNonNull(documentSnapshot.get("email")).toString());
                             BioTextView.setText(Objects.requireNonNull(documentSnapshot.get("bio")).toString());
 
-                            Map<String, Boolean> friendMap = 
-                                    (Map<String, Boolean>)documentSnapshot.get("friends");
-
-                            if (friendMap != null) {
-                                NumberOfFriends.setText(String.valueOf(friendMap.size()));
+                            try {
+                                Map<String, Boolean> friendMap =
+                                        (Map<String, Boolean>) documentSnapshot.get("friends");
+                                if (friendMap != null) {
+                                    NumberOfFriends.setText(String.valueOf(friendMap.size()));
+                                }
+                            } catch (Exception innner) {
+                                NumberOfFriends.setText(String.valueOf(0));
                             }
 
                             Object pic_url;
